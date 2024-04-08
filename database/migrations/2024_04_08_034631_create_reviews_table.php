5c51c2b2c0f1aa003a7acdaf854ab9f4c4133077
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-04-07 23:32:36 -0500

    Create Book model and migration

--- database/migrations/2024_04_08_034631_create_reviews_table.php
+++ database/migrations/2024_04_08_034631_create_reviews_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+
+            $table->text('review');
+            $table->unsignedTinyInteger('rating');
+
             $table->timestamps();
         });
     }
