216857f2db9935545701b1cf7409460a44e841b1
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-04-24 23:48:33 -0500

    Create labels for filters

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -9,6 +9,22 @@
         <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
     </form>
 
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular last month',
+                'popular_last_6months' => 'Popular last 6 months',
+                'highest_rated_last_month' => 'Highest Rated last month',
+                'highest_rated_last_6months' => 'Highest Rated last 6 months',
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+            <a href="#" class="filter-item">{{ $label }}</a>
+        @endforeach
+    </div>
+
     <ul>
         @forelse ($books as $book)
             <li class="mb-4">
