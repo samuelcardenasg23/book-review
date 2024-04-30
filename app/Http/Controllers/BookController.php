df52878c37d2a27ed8473c548f69b5cde7eaa405
Samuel Cardenas <samuelcardenas1706@gmail.com>
2024-04-29 22:59:23 -0500

    Sort reviews by latest in book page

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -54,7 +54,14 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view('books.show', ['book'=> $book]);
+        return view(
+            'books.show',
+            [
+                'book'=> $book->load([
+                    'reviews' => fn ($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
